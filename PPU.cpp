403a164e1caf997245189806af7357159aa4236c
Sagiv440 <91212819+Sagiv440@users.noreply.github.com>
2021-10-12 08:39:48 +0300

    Added the file for the NES graphics chip

---
+++ PPU.cpp
@@ -0,0 +1,173 @@
+#include "PPU.h"
+
+NES_PPU::NES_PPU(uint16_t top = 0xffff, uint16_t buttom = 0x0000, BUS* module_bus = NULL)
+{
+	TOP_ADDRESS = top;
+	BUTTOM_ADDRESS = buttom;
+	bus = module_bus;
+};
+NES_PPU::~NES_PPU() {};
+
+void NES_PPU::save_data(uint16_t address, uint8_t &data)
+{
+	bus->ADDRESS_BUS = address;
+	bus->DATA_BUS = data;
+	bus->save();
+}
+uint8_t NES_PPU::load_data(uint16_t address)
+{
+	bus->ADDRESS_BUS = address;
+	bus->load();
+	return bus->DATA_BUS;
+}
+
+uint8_t NES_PPU::send_data(uint16_t& address)
+{
+	switch (address & 0x0007)  // using the last 3 bit of the address bus to select the correct registor
+	{
+	case PPUSTATUS:
+		return status;
+		break;
+
+	case OAMDATA:
+		return OAM_memory[OAM_address];
+		break;
+
+	case PPUDATA:
+		return load_data(D_address);
+		break;
+
+	default:
+		return 0x00;
+		break;
+	}
+	
+	// Status ($2002) < read
+	// OAM data ($2004) <> read/write
+	// Data ($2007) <> read/write
+
+};
+void NES_PPU::receive_data(uint16_t& address, uint8_t& data)
+{
+	switch (address & 0x0007)  // using the last 3 bit of the address bus to select the correct registor
+	{
+	case PPUCTRL:
+		control = data;
+		w_1 = 0;
+		break;
+
+	case PPUMASK:
+		mask = data;
+		w_1 = 0;
+		break;
+
+	case OAMADDR:
+		OAM_address = data;
+		w_1 = 0;
+		break;
+
+	case OAMDATA:
+		OAM_memory[OAM_address] = data;
+		w_1 = 0;
+		break;
+
+	case PPUSCROLL:
+		// Scrooling is on a brack
+		if (w_1 == 0)
+		{
+			//Change the cam_X position
+			X_cam = data;
+			w_1 = 1;
+		}
+		else
+		{
+			//Change the cam_Y position
+			Y_cam = data;
+			w_1 = 0;
+		}
+		break;
+		break;
+
+	case PPUADDR:
+		if (w_1 == 0)
+		{
+			D_address = (uint16_t)data << 8;
+			D_address &= 0xff00;
+			w_1 = 1;
+		}
+		else
+		{
+			D_address = D_address | data;
+			w_1 = 0;
+		}
+		break;
+
+	case PPUDATA:
+		save_data(D_address, data);
+		w_1 = 0;
+		break;
+
+	case OAMDMA:
+
+		//Activate_DMA_protocol
+
+		w_1 = 0;
+		break;
+
+	default:
+		w_1 = 0;
+		break;
+	}
+	// Controlle ($2000) > write
+	// Mask ($2001) > write
+	// OAM address ($2003) > write
+	// OAM data ($2004) <> read/write
+	// Scroll ($2005) >> write x2
+	// Address ($2006) >> write x2
+	// Data ($2007) <> read/write
+	// OAM DMA ($4014) > write
+
+
+}
+void NES_PPU::interrupt(uint8_t& contrl) {};
+
+void NES_PPU::pattern(uint8_t &P)
+{
+	uint8_t pixel, p0, p1;
+	for (uint8_t y; y < 8; y++) {
+		p0 = load_data((uint16_t)P * 8 + y);
+		p1 = load_data((uint16_t)P * 8 + y + 0x0800);
+		for (uint8_t x; x < 8; x++) {
+			pixel =   (p0 & (0xfe << x)) != 0;
+			pixel += ((p1 & (0xfe << x)) != 0)? 2:0 ;
+
+		}
+	}
+}
+
+void NES_PPU::BG_render()
+{
+
+}
+
+void NES_PPU::FG_render()
+{
+
+}
+
+void NES_PPU::Execute() 
+{
+	// Chack control register
+	// Scroling
+	// Rendering
+}
+
+void NES_PPU::Scroling()
+{
+
+}
+ 
+void NES_PPU::Rendering()
+{
+	
+}
---
+++ PPU.h
@@ -0,0 +1,106 @@
+#pragma once
+#include "BUS.h"
+
+#define PPUCTRL 0	 // Controlle ($2000) > write
+#define	PPUMASK 1	 // Mask ($2001) > write
+#define	PPUSTATUS 2	 // Status ($2002) < read
+#define OAMADDR 3	 // OAM address ($2003) > write
+#define OAMDATA 4	 // OAM data ($2004) <> read/write
+#define PPUSCROLL 5	 // Scroll ($2005) >> write x2
+#define PPUADDR 6	 // Address ($2006) >> write x2
+#define PPUDATA 7	 // Data ($2007) <> read/write
+#define OAMDMA 8	 // OAM DMA ($4014) > write
+
+
+class NES_PPU : public module
+{
+	//register 0
+	enum Ctrl
+	{
+		V_NMI	= 0x80,	//		when the bit 7 is clear "bit = 0" the PPU will not send and nmi interrupt to the cpu whenever a V-Blank occures.
+		M_S		= 0x40,	//		PPU Master Slave_slect: (0: read backdrom form EXT pin: 1: output color on EXT pins)
+		S_S		= 0x20,	//		Sprite size (0: 8x8 pixels; 1: 8x16 pixels)
+		B_P		= 0x10,	//		Background pattern table address (0: $0000; 1: $1000)
+		S_P		= 0x08,	//		Sprite pattern table address for 8x8 sprites (0: $0000; 1: $1000; ignored in 8x16 mode)
+		VRAM_A	= 0x04,	//		VRAM address increment per CPU read / write of PPUDATA (0: add 1, going across; 1: add 32, going down)
+		NN		= 0x03	//		Base nametable address (0 = $2000; 1 = $2400; 2 = $2800; 3 = $2C00)
+	};
+	//register 1
+	enum Mask
+	{
+		B		= 0x80, // Emphasize blue
+		G		= 0x40, // Emphasize green (red on PAL/Dendy)
+		R		= 0x20, // Emphasize red (green on PAL/Dendy)
+		s		= 0x10, // 1: Show sprites
+		b		= 0x08, // 1: Show background
+		M		= 0x04, // 1: Show sprites in leftmost 8 pixels of screen, 0: Hide
+		m		= 0x02, // 1: Show background in leftmost 8 pixels of screen, 0: Hide
+		Gr		= 0x01  // Greyscale (0: normal color, 1: produce a greyscale display)
+	};
+	//register 2
+	enum Status
+	{
+		V_Blank = 0x80, // Vertical blank has started (0: not in vblank; 1: in vblank).
+						// Set at dot 1 of line 241 (the line *after* the post - render
+						// line); cleared after reading $2002 and at dot 1 of the
+						// pre - render line.
+		S_h		= 0x40, // Sprite 0 Hit.  Set when a nonzero pixel of sprite 0 overlaps
+						// a nonzero background pixel; cleared at dot 1 of the pre - render
+						// line.Used for raster timing.
+		S_o		= 0x20, // Sprite overflow. The intent was for this flag to be set
+						// whenever more than eight sprites appear on a scanline, but a
+						// hardware bug causes the actual behavior to be more complicated
+						// and generate false positives as well as false negatives; see
+						// PPU sprite evaluation.This flag is set during sprite
+						// evaluation and cleared at dot 1 (the second dot) of the
+						// pre - render line.
+	};
+
+	BUS* bus;
+	uint8_t w_1 = 0x00;
+	uint8_t control = 0x00;
+	uint8_t mask = 0x00;
+	uint8_t status = 0x00;
+	uint8_t OAM_address = 0x00;
+	uint16_t D_address = 0x0000;
+	uint8_t X_cam = 0x00;
+	uint8_t Y_cam = 0x00;
+
+	uint8_t OAM_memory[0x100] = { 0x00 };
+
+	uint8_t* screen_buffer;
+
+	void save_data(uint16_t address, uint8_t &data);
+	uint8_t load_data(uint16_t address);
+
+	void pattern(uint8_t &P);
+
+	// Incharge of rendering the background frame
+	void BG_render();
+
+	//Incharge of rendering the Sprites in the farme
+	void FG_render();
+	
+public:
+	 
+
+	NES_PPU(uint16_t top, uint16_t buttom, BUS* module_bus);
+	~NES_PPU();
+
+	void Rest();
+	void Execute();
+	void Scroling();
+	void Rendering();
+
+	void set_bus(BUS* main_bus)
+	{
+		bus = main_bus;
+	}
+
+	uint8_t send_data(uint16_t& address);
+	void receive_data(uint16_t& address, uint8_t& data);
+	void interrupt(uint8_t& contrl);
+
+
+	
+};
